bc7444e8cc69b2e406de4b3478d5f98b5f3ccc18
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2021-03-26 09:21:51 -0300

    Implementando a lógica de filtragem

--- Js-LinguagemWeb/js/filtro.js
+++ Js-LinguagemWeb/js/filtro.js
@@ -1,10 +1,26 @@
+//lógica de filtragem da tabela
 let campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
   let pacientes = document.querySelectorAll(".paciente");
-  for(var i = 0; i< paciente.length; i++){
-    let paciente = pacientes[i];
-    let tdNome = paciente.querySelector(".info-nome");
-    let nome = paciente.textContent;
+
+  if(this.value.length>0){
+    for(var i = 0; i< paciente.length; i++){
+      let paciente = pacientes[i];
+      let tdNome = paciente.querySelector(".info-nome");
+      let nome = tdNome.textContent;
+  
+      if(nome != this.value){
+        paciente.classList.add("invisivel");
+      }else{
+        paciente.classList.remove("invisivel");
+      }
+    }
+  }else{
+    for(var i = 0; i<pacientes.length;i++){
+      let paciente = pacientes[i];
+      paciente.classList.remove("invisivel");
+    }
   }
+  
 });
