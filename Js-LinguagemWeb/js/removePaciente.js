73664a219d017899f5a9abd7b3c8d3de3ec2c34b
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2021-03-25 22:46:02 -0300

    Fade out da remoção do paciente

--- Js-LinguagemWeb/js/removePaciente.js
+++ Js-LinguagemWeb/js/removePaciente.js
@@ -5,9 +5,13 @@ tabela.addEventListener("dblclick", function(event){
   let alvoEvento = event.target;
   let paiDoAlvo = alvoEvento.parentNode;
 
-  //removendo o pai do td
-  paiDoAlvo.remove();
+  //adicionando a classe pra remover com fade out
+  paiDoAlvo.classList.add("fadeOut");
+//removendo o pai do td com time out que "segura" o código por 500ms
+  setTimeout(function(){
+    paiDoAlvo.remove();
+  }, 500);
 
-   
+  
 });
 
